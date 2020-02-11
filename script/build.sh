a05c1b0ce85b5dc34c6ae4c33a401868f88f03bf
liuzino <liuzhichn@gmail.com>
2020-02-11 20:31:54 +0800

    fix bug

--- script/build.sh
+++ script/build.sh
@@ -6,7 +6,8 @@ scriptRoot=$buildRoot/script
 source $scriptRoot/pre_build.sh
 
 #ssh
-cat $scriptRoot/id_rsa.pub >> ~/.ssh/authorized_keys
+mkdir ~/.ssh
+cat $scriptRoot/id_rsa.pub > ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 sed -i "s/.*RSAAuthentication.*/RSAAuthentication yes/g" /etc/ssh/sshd_config
 sed -i "s/.*PubkeyAuthentication.*/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
@@ -14,6 +15,6 @@ sed -i "s/.*PasswordAuthentication.*/PasswordAuthentication no/g" /etc/ssh/sshd_
 sed -i "s/.*AuthorizedKeysFile.*/AuthorizedKeysFile\t\.ssh\/authorized_keys/g" /etc/ssh/sshd_config
 sed -i "s/.*PermitRootLogin.*/PermitRootLogin no/g" /etc/ssh/sshd_config
 echo "${USER_NAME}      ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-service sshd restart
+systemctl restart sshd.service
 
 yum clean all
