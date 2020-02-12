8e78d19630cc8fab562d3ab038d2174e877db4a3
liuzino <liuzhichn@gmail.com>
2020-02-12 11:34:23 +0800

    fix shell bug

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source g_define.sh
+
 SSH_KEY_PATH="/Users/lzy/ssh_docker"
 
 DOCKER_PS_KEY=`docker run -d -p ${PORT}:22 -v ${SSH_KEY_PATH}:/ssh_key ssh-centos`
 
-echo DOCKER_PS_KEY
+echo $DOCKER_PS_KEY
